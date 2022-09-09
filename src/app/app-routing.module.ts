76c4e05c91d201b91efdea0e0204b7feb7f51498
oyebolasimeon <simeonoyekunleoyebola@gmail.com>
2022-09-09 10:58:33 +0100

    verify account

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,6 +8,7 @@ import { RegisterComponent } from './Auth/register/register.component';
 import { JoinThriftComponent } from './dashboard/thrift/join-thrift/join-thrift.component';
 import { CreateThriftsComponent } from './dashboard/thrift/create-thrifts/create-thrifts.component';
 import { ThriftlogsComponent } from './dashboard/thrift/thriftlogs/thriftlogs.component';
+import { ProfileComponent } from './profile/profile.component';
 
 const routes: Routes = [
   {path: 'login', component: SigninComponent},
@@ -17,6 +18,7 @@ const routes: Routes = [
   { path: "create-thrift", component: CreateThriftsComponent},
   { path: "join-thrift", component: JoinThriftComponent},
   {path: "logs", component: ThriftlogsComponent},
+  { path: "myprofile", component: ProfileComponent},
   {path: '', redirectTo: '/login', pathMatch: 'full'},
   {path: '**', redirectTo: '/myboard'}
 ];
--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -7,13 +7,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
+  greet: any;
+  verification: boolean = false
   constructor() { }
 
   ngOnInit(): void {
+    this.greeting();
   }
 
   back(){
     history.back();
+  }
+
+  greeting(){
+    const day = new Date();
+    const hour = day.getHours();
+
+    if(hour >0 && hour <12){
+      this.greet = "Morning"
+    }
+    else if(hour >12 && hour <15){
+      this.greet = "Afternoon"
+    } else{
+      this.greet = "Evening"
+    }
+
+    return this.greet
+  }
+
+
+  verifyAccount(){
+    if(this.verification == false){
+      this.verification = true;
+    } else if(this.verification == true) {
+      this.verification = false
+    }
+   
+    return this.verification
+  }
+
+  verification(){
+    
   }
 
 
