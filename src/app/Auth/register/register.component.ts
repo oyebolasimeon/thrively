babc47f36b5435388eafe09e01df7f59c8f8bc14
dev-thrively <dev.thrivey@gmail.com>
2022-10-15 03:05:24 +0100

    user onboarding and validation

--- src/app/Auth/register/register.component.ts
+++ src/app/Auth/register/register.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { AuthService } from 'src/app/Service/auth.service';
 
 @Component({
   selector: 'app-register',
@@ -7,10 +9,39 @@ import { Router } from '@angular/router';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  country: any;
 
-  constructor(private route: Router) { }
+  constructor(private route: Router, private Services: AuthService) { }
 
   ngOnInit(): void {
+    this.getAllCountry();
+  }
+
+  Register = new FormGroup({
+    firstname: new FormControl(" ", Validators.required),
+    lastname: new FormControl(" ", Validators.required),
+    phonenumber: new FormControl(" ", Validators.required),
+    email: new FormControl(" ", Validators.email),
+    address: new FormControl(" ", Validators.required),
+    country: new FormControl(" ", Validators.required),
+    password: new FormControl(" ", Validators.required)
+
+  })
+
+
+  signUp(){
+    if(this.Register.valid){
+      const payload = this.Register.getRawValue();
+      console.log(payload)
+      // this.Services.createNewUser()
+    }
+
+  }
+
+  getAllCountry(){
+    this.Services.getCountries().subscribe((res: any) => {
+      this.country = res.data;
+    })
   }
 
   back(){
