a944c85345de9913ee2cf95c1d352d1fd3337ff0
dev-thrively <dev.thrivey@gmail.com>
2022-10-15 03:51:13 +0100

    user nboarding otp section

--- src/app/Auth/register/register.component.ts
+++ src/app/Auth/register/register.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/Service/auth.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-register',
@@ -22,6 +23,14 @@ export class RegisterComponent implements OnInit {
     lastname: new FormControl(" ", Validators.required),
     phonenumber: new FormControl(" ", Validators.required),
     email: new FormControl(" ", Validators.email),
+    othername: new FormControl(""),
+    mothername: new FormControl(""),
+    gender: new FormControl(""),
+    date_of_birth: new FormControl(" ", Validators.required),
+    zipcode: new FormControl(""),
+    city: new FormControl(""),
+    state: new FormControl(""),
+    user_image: new FormControl(""),
     address: new FormControl(" ", Validators.required),
     country: new FormControl(" ", Validators.required),
     password: new FormControl(" ", Validators.required)
@@ -33,7 +42,10 @@ export class RegisterComponent implements OnInit {
     if(this.Register.valid){
       const payload = this.Register.getRawValue();
       console.log(payload)
-      // this.Services.createNewUser()
+      this.Services.createNewUser(payload).subscribe((res:any) => {
+        console.log(res)
+        this.otp()
+      })
     }
 
   }
@@ -53,6 +65,44 @@ export class RegisterComponent implements OnInit {
   }
   navigatenewUser(){
     this.route.navigate(['/login'])
+  }
+
+  otp(){
+    Swal.fire({
+      title: 'Paste OTP here',
+      input: 'text',
+      inputAttributes: {
+        autocapitalize: 'off'
+      },
+      showCancelButton: false,
+      confirmButtonText: 'Submit',
+      showLoaderOnConfirm: true,
+      preConfirm: (login) => {
+        return fetch(`//api.github.com/users/${login}`)
+          .then(response => {
+            if (!response.ok) {
+              throw new Error(response.statusText)
+            }
+            return response.json()
+          })
+          .catch(error => {
+            Swal.showValidationMessage(
+              `Request failed: ${error}`
+            )
+          })
+      },
+      allowOutsideClick: () => !Swal.isLoading()
+    }).then((result) => {
+      if (result.isConfirmed) {
+        Swal.fire({
+          position: 'top-end',
+          icon: 'success',
+          title: 'OTP Confirmed',
+          showConfirmButton: false,
+          timer: 1500
+        })
+      }
+    })
   }
   
 
