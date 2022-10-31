caf2626a8a686be0e3b8dfe4d69e3821f51ac5d1
dev-thrively <dev.thrivey@gmail.com>
2022-11-01 00:48:42 +0100

    editig

--- src/app/dashboard/dashboard-routing.module.ts
+++ src/app/dashboard/dashboard-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProfileComponent } from '../profile/profile.component';
 import { AuthguardGuard } from '../Shared/authguard.guard';
+import { BoardComponent } from './board/board.component';
 import { DashboardComponent } from './dashboard.component';
 
 const routes: Routes = [
@@ -9,8 +10,9 @@ const routes: Routes = [
     path: '', 
     component: DashboardComponent, 
     children: [
+      {path: 'home', component: BoardComponent, canActivate: [AuthguardGuard]},
       {path: 'myprofile', component: ProfileComponent, canActivate: [AuthguardGuard]},
-    { path: '', redirectTo: 'myprofile', pathMatch: 'full'},
+    { path: '', redirectTo: 'home', pathMatch: 'full'},
   ]}
   
 ];
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ProfileService } from '../profile/profile.service';
+import { AuthService } from '../Service/auth.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,9 +9,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
-
+  constructor( private service: ProfileService ) { }
+  userName: any;
+  greet: any;
+  barVisible = true;
   ngOnInit(): void {
+    this.greeting();
+    this.getAccountName();
+  }
+
+  getAccountName(){
+    let payload = localStorage.getItem("accountID")
+    return this.service.getUserDetails(payload).subscribe((data: any) => {
+      this.userName =  data.result[0].firstname
+    })
+  }
+
+  removeBar(){
+    this.barVisible = false;
+  }
+
+  greeting(){
+    const day = new Date();
+    const hour = day.getHours();
+
+    if(hour >0 && hour <12){
+      this.greet = "Morning"
+    }
+    else if(hour >12 && hour <15){
+      this.greet = "Afternoon"
+    } else{
+      this.greet = "Evening"
+    }
+
+    return this.greet
   }
 
 }
