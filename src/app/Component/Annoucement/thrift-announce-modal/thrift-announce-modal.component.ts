dc20c76b20c30ff3b5a85761d9ce525af178e02b
dev-thrively <dev.thrivey@gmail.com>
2022-11-03 09:42:23 +0100

    dahboard Fix

---
+++ src/app/Component/Annoucement/thrift-announce-modal/thrift-announce-modal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-thrift-announce-modal',
+  templateUrl: './thrift-announce-modal.component.html',
+  styleUrls: ['./thrift-announce-modal.component.css']
+})
+export class ThriftAnnounceModalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
