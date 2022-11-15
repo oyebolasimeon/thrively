6d8df410dcfc7fb91ac6af88de284707ffee462f
oyebolasimeon <simeonoyekunleoyebola@gmail.com>
2022-11-15 03:35:12 +0100

    domestic thrift

--- src/app/dashboard/thrift-main-board/domestic-thrift/domestic-thrift.component.ts
+++ src/app/dashboard/thrift-main-board/domestic-thrift/domestic-thrift.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-domestic-thrift',
@@ -7,9 +8,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DomesticThriftComponent implements OnInit {
 
+  email: any = localStorage.getItem("email")
+  accountID: any = localStorage.getItem("accountID");
+
   constructor() { }
 
   ngOnInit(): void {
   }
+
+
+  createThrift = new FormGroup({
+    thriftName: new FormControl(" ", Validators.required),
+    description: new FormControl(" ", Validators.required),
+    amount: new FormControl(" ", Validators.required),
+    numberOfPeople: new FormControl(" ", Validators.required),
+    // email: this.email,
+    // accountID: this.accountID,
+    startDate: new FormControl(" ", Validators.required),
+    duration: new FormControl(" ", Validators.required)
+  })
+
+  submitThrift(){
+    if(this.createThrift.valid){
+      const payload = this.createThrift.getRawValue();
+      payload.email = this.email;
+      payload.accountID = this.accountID
+
+      console.log(payload)
+    }
+  }
+
 
 }
