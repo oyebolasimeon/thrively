8a3a79979ef58b75cd82dad188fc09cb66f61620
simeonoyebola <simeon.oyebola@kibo.school>
2022-11-23 18:38:26 +0100

    thrift table

--- src/app/dashboard/thrift/thriftlogs/thriftlogs.component.ts
+++ src/app/dashboard/thrift/thriftlogs/thriftlogs.component.ts
@@ -1,3 +1,4 @@
+import { ProfileService } from './../../../profile/profile.service';
 import { ThriftService } from './../thrift.service';
 import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
 // import {  } from 'stream';
@@ -11,23 +12,39 @@ export class ThriftlogsComponent implements OnInit {
 
   // @Output() thriftType = new EventEmitter<any>();
   @Input() thriftType: any;
+  name: any;
   p: number = 1;
+  userDetails: any;
 
   accNumber = localStorage.getItem("accountID");
-  thriftlog: any;
-  constructor( private thriftService: ThriftService ) { }
+  thriftlog : any;
+  constructor( private thriftService: ThriftService, private userService: ProfileService ) { }
 
   ngOnInit(): void {
     this.fetchAllThrift();
   }
 
-  fetchAllThrift(){
+  public fetchAllThrift(): any{
     const payload = this.accNumber;
+    let log;
     this.thriftService.fetchAllThrift(payload).subscribe((res:any) =>{
-      this.thriftlog = res
+      this.thriftlog = res.result
       console.log("Logs  ",this.thriftlog)
+      // return this.thriftlog;
     })
   }
+
+  // getThriftCreator(id: any){
+  //   let payload = id;
+  //   this.userService.getUserDetails(payload).subscribe((data: any) => {
+  //     this.userDetails = data.result[0].firstname + ' ' + data.result[0].lastname;
+  //     // console.log(this.userDetails)
+  //     // return this.userDetails;
+      
+  //   })
+  // }
+
+
 
   back(){
     history.back();
