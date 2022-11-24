d8b1bcca8bef2409710cfc0809f4b9b98f103c64
simeonoyebola <simeon.oyebola@kibo.school>
2022-11-24 09:51:31 +0100

    notification settings

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,3 +1,4 @@
+import { AuthService } from 'src/app/Service/auth.service';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
@@ -7,12 +8,25 @@ import { Router } from '@angular/router';
   styleUrls: ['./nav-bar.component.css']
 })
 export class NavBarComponent implements OnInit {
-
+  accountID = localStorage.getItem("accountID");
+  notifications: any;
+  notifyLength: any;
   constructor(
-    private router: Router
+    private router: Router,
+    private service: AuthService
   ) { }
 
   ngOnInit(): void {
+    this.getNotification();
+  }
+
+  getNotification(){
+    const payload = this.accountID;
+    this.service.getNotification(payload).subscribe((res:any) =>{
+      this.notifications = res.result.slice(0,5);
+      this.notifyLength = res?.result.length;
+      console.log(this.notifyLength);
+    })
   }
 
   clearAll(){
